c3538c2e0a26c913bf26a8b723456db2ba81d81e
Sonali Merchia <sonalimerchia@yahoo.com>
2021-01-18 18:10:47 -0800

    update gitignore

---
+++ server/src/mikro-orm.config.js
@@ -0,0 +1,22 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // typescript or javascript
+  },
+  entities: [ Post, User],
+  username: 'postgres',
+  password: 'Anjali12',
+  host: 'localhost',
+  dbName: 'wreddit',
+  type: 'postgresql',
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
+
+// Last line exports the object as if it were the first set of parameters
+// for MikroORM.init()
